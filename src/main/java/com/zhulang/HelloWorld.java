1abdc08f1827d3d7e8a53077f8a3b95bdacefb3e
狐狸半面添 <3179167073@qq.com>
2023-10-07 15:34:00 +0800

    创建了 HelloWorld 类

---
+++ src/main/java/com/zhulang/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.zhulang;
+
+/**
+ * Description:
+ *
+ * @Author 狐狸半面添
+ * @Create 2023/10/7 15:32
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
